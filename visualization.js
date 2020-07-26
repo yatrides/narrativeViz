8bb65969a720a7b86a4809c4f0ea6ac9193f03d7
yatrides <k.yatrides@gmail.com>
2020-07-26 01:03:57 -0700

    test

--- visualization.js
+++ visualization.js
@@ -1,5 +1,5 @@
-function showFirstScene(){
+async function init(){
     d3.select("#chart").append("svg").attr("height",500).attr("width",1200);
     const data= await d3.csv("data/regional-averages-of-the-composite-gender-equality-index.csv");
-    console.log(data);
+    console.log(data);  
 }
