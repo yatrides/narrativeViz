9a7ad1110912d6d2c013a85a6944cae144074182
yatrides <k.yatrides@gmail.com>
2020-07-26 18:40:53 -0700

    axis

--- visualization.js
+++ visualization.js
@@ -14,7 +14,7 @@ async function init(){
         .attr("transform",
               "translate(" + margin.left + "," + margin.top + ")");
 
-    const data= await d3.csv("data/region-averages-of-the-composite-gender-equality-index.csv");
+    const data= await d3.csv("data/women_dataset.csv");
     console.log(data);  
     
     //list of groups 
