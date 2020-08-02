158a0ffde65a883c817e7a01c074fc02f4a771f5
yatrides <k.yatrides@gmail.com>
2020-08-02 00:31:50 -0700

    Change of color and title

--- visualization.js
+++ visualization.js
@@ -41,6 +41,7 @@ async function init(orderType){
  //.append("svg")
    .attr("width", width + margin.left + margin.right)
    .attr("height", height + margin.top + margin.bottom)
+   .append("g")
    .attr("transform",
          "translate(" + margin.left + "," + margin.top + ")");
 
